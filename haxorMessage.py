d6db006f8a6f23cb068c91f6509a2c89e5fe343c
Robin G. Aaberg <robin.garen@gmail.com>
2012-03-04 01:39:39 +0100

    the python server

---
+++ haxorMessage.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+#
+#	@author: technocake
+#	a hackers(the movie)-like interface to give battle cries to your nemesis
+#	BIG LETTERS WARNING
+#
+##############################################
+
+import socket, sys
+
+HOST, PORT = '', 8888
+
+s = socket.socket()
+
+s.bind((HOST, PORT))
+s.listen(1)
+
+HTML_HEAD = """HTTP/1.1 200
+
+<!DOCTYPE html>
+<style>
+	h1 { color: red; }
+</style>
+"""
+MSG_WRAPS = """
+<h1>%s</h1>
+"""
+try:
+	while 1:		
+		(cs, ip) = s.accept()
+		print ( cs.recv(32000) )
+
+		while 1:
+			cs.send( HTML_HEAD)
+			line = raw_input("msg> ")
+			cs.send(MSG_WRAPS%(line,))
+except Exception as e:
+	s.close()
+	print e
